424300a5e0332903da580a68b74083ac1baebe24
IAPeter07 <138104461+IAPeter07@users.noreply.github.com>
2024-06-03 10:08:31 -0300

    aula 2

---
+++ Aula02/estruturas.php
@@ -0,0 +1,15 @@
+<?php
+    //Comentarios
+
+    // '//' --> Comentarios Normais/de uma Linha
+    // '/*   ...  */' --> Comentarios em Blocos
+
+    //Variaveis
+    $numero = 0; //numero Inteiro (Int)
+    $nome = "IA";//String
+
+    $str = "5" + 10; //Concatenação (soma valores de tipos diferentes)
+    $num = "5". 10; //Apenas Junta as Variaveis
+    echo $str."<br>";
+    echo $num."<br>";
+?>
