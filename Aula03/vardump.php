345fd7a390da291737c7bba3ca33243080869d48
IAPeter07 <138104461+IAPeter07@users.noreply.github.com>
2024-06-05 10:30:50 -0300

    Add Cod de Teste da função var_dump()

---
+++ Aula03/vardump.php
@@ -0,0 +1,10 @@
+<?php 
+    $numero = 123;
+    $texto = "ola, mundo!";
+    $array = [1,2,3];
+
+    //Var dump da a descrição do que esta dentro da Variavel
+    var_dump($numero)."<br>";
+    var_dump($texto)."<br>";
+    var_dump($array)."<br>";
+?>
