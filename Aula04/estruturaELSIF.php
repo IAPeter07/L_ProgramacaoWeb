5e69165763c4da2fa385618e57672835b87698e8
IAPeter07 <138104461+IAPeter07@users.noreply.github.com>
2024-06-07 09:38:16 -0300

    Aula04

---
+++ Aula04/estruturaELSIF.php
@@ -0,0 +1,12 @@
+<?php 
+    $idade = 18;
+    if($idade >18){
+        echo("Vc é maior de idade");
+    }
+    elseif($idade == 18){
+        echo("Vc acabo de atingir a maturidade");
+    }
+    else{
+        echo("Vc é menor de Idade");
+    }
+?>
