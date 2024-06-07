61e89f151f162dbf1d7aee8fb853a6c0699d236d
IAPeter07 <138104461+IAPeter07@users.noreply.github.com>
2024-06-07 10:10:42 -0300

    Update estruturaSWITCH.php

--- Aula04/estruturaSWITCH.php
+++ Aula04/estruturaSWITCH.php
@@ -1,15 +1,21 @@
 <?php
+    //Variavel 'Hoje' recebe o valor 'Quarta'
     $hoje = "Quarta";
+    //Se a variavel ($hoje) for igual á
     switch ($hoje){
+        //Caso 1
         case "Segunda":
             echo("Hoje é Segunda-Feira");
             break;
+        //Caso 2
         case "Terça":
             echo("Hoje é Terça-Feira");
             break;
+        //Caso 3
         case"Quarta":
             echo("Hoje é Quarta-Feira");
             break;
+        //Mensagem Padrão
         default:
             echo("Não é inicio de Semana");
     }
