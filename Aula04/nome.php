42533a6f3a0c70610ed8112d0584acd83def73f3
IAPeter07 <138104461+IAPeter07@users.noreply.github.com>
2024-06-07 10:00:44 -0300

    Update nome.php

--- Aula04/nome.php
+++ Aula04/nome.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
+    //Variavel Nome recebe valor
     $nome = "Till Lindermann";
+    //SE a variavel($nome) for igua á:
     if($nome == "Richard Kruspe"){
+        //IMPRIMA
         echo("E ai Richard Kruspe");
     }
+    //OU ENTÂO SE a variavel($nome) for igua á:
     elseif($nome == "Oliver Riedel"){
+        //IMPRIMA
         echo("E ai Oliver Riedel");
     }
+    //OU ENTÂO SE a variavel($nome) for igua á:
     elseif($nome == "Till Lindermann"){
+        //IMPRIMA
         echo("E ai Till Lindermann");
     }
+    //SE NÃO for nenhuma das opções Anteriores
     else{
         echo("E ai $nome");
     }
