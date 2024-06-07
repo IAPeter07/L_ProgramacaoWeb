99ba261171aa68323c2ad9acec9ba5d92db1eac9
IAPeter07 <138104461+IAPeter07@users.noreply.github.com>
2024-06-07 10:13:26 -0300

    Update estruturaIF_ELSE.php

--- Aula04/estruturaIF_ELSE.php
+++ Aula04/estruturaIF_ELSE.php
@@ -7,20 +7,29 @@
 </head>
 <body>
     <?php 
+        //Variavel idade recebe valor '17'
         $idade = 17;
+        //SE a variavel ($idade) forr igual a '17'
         if($idade < 18){
+            //IMPRIMA
             echo("Vc não pode entrar");
         }
+        //SE NÂO..
         else{
+            //IMPRIMA
             echo("Seja Bem-vindo(a)");
         }
     
         $idade = 23;
         $identidade = true;
+        //SE a variavel ($idade) for maior que '18' E a variavel($identidade) For'VERDADEIRA'
         if ($idade > 18 && $identidade == true){
+            //IMPRIMA
             echo("Seja Bem-vindo");
         }
+        //SE NÂO..
         else{
+            //IMPRIMA
             echo("Vc não pode entrar");
         }
     ?>
