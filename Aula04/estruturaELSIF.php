050c3419eb4d27160967f8269b48858bdcfd94b9
IAPeter07 <138104461+IAPeter07@users.noreply.github.com>
2024-06-07 10:07:37 -0300

    Update estruturaELSIF.php

--- Aula04/estruturaELSIF.php
+++ Aula04/estruturaELSIF.php
@@ -1,12 +1,19 @@
 <?php 
+    //Varivael idade recebe valor '18'
     $idade = 18;
+    //SE a Variavel($idade) Seja maior que 18
     if($idade >18){
+        //IMPRIMA
         echo("Vc é maior de idade");
     }
+    //Ou então Caso a Variavel($idade) Seja maior que 18
     elseif($idade == 18){
+        //IMPRIMA
         echo("Vc acabo de atingir a maturidade");
     }
+    //SE NÂO
     else{
+        //IMPRIMA
         echo("Vc é menor de Idade");
     }
 ?>
