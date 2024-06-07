95806ed4c0af345d8c86704c47eec9ef35489ecd
IAPeter07 <138104461+IAPeter07@users.noreply.github.com>
2024-06-07 10:04:12 -0300

    Update identidade.php

--- Aula04/identidade.php
+++ Aula04/identidade.php
@@ -7,17 +7,23 @@
 </head>
 <body>
     <?php
+        //Variavel $nome recebe valor 'Fulano'
         $nome = Fulano;
+        //Se a variavel $nome for
         switch($nome){
+            //Caso 1
             case"Fulano":
-                echo("E ai Fulano");
-                break;
+                echo("E ai Fulano"); //IMPRIMA
+                break; //ENCERRA
+            //Caso 2
             case"Citrano":
                 echo("E ai Citrano");
                 break;
+            //Caso 3
             case"Beltrano":
                 echo("E ai Beltrano");
                 break;
+            //Mensagem Padrão
             default:
                 echo("Qual é seu nome");
                 break;
