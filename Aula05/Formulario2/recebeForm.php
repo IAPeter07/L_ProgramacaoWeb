53cc80c91f1ce9e9fc165ddaecd0833d8fcc2a27
IAPeter07 <138104461+IAPeter07@users.noreply.github.com>
2024-06-10 10:40:52 -0300

    Adicionando Cod. recebeForm

---
+++ Aula05/Formulario2/recebeForm.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            echo("O valor de Campo 1 é: " . $_POST["campo1"]);
+            echo("<br>O valor do Campo 2 é: " . $_POST["campo2"]);
+        ?>
+    </body>
+</html>
