4a9b9528c90d3eabad90d0ad0d77780ee16268c5
IAPeter07 <138104461+IAPeter07@users.noreply.github.com>
2024-06-10 08:22:34 -0300

    Adicionando Cod. Aula 5

---
+++ Aula05/estruturaFOR.php
@@ -0,0 +1,7 @@
+<?php
+    #varivel ($i) ínicia com o Valor '1'
+    #essa varrivale ira incrementar 1 e imprimir até chegar o valor igual 5
+    for($i=1; $i<=5; $i++){
+        echo("Numero:".$i."<br");
+    }
+?>
